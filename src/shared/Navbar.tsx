26a4563ec0c60a7d094a9b0c680ca52a03be5c31
danursin <danursin@gmail.com>
2019-11-17 00:16:40 -0600

    navbar username

--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -13,18 +13,16 @@ const Navbar: React.FC = () => {
                 as={Link}
                 to="/dashboard"
                 header
-                content={<Image src={pot} style={{ height: "2rem" }} />}
+                content={
+                    <>
+                        <Image src={pot} style={{ height: "2rem", paddingRight: "1rem" }} />
+                        {!!user && <span>Welcome {user.Name}</span>}
+                    </>
+                }
                 position="left"
                 style={{ paddingTop: 0, paddingBottom: 0 }}
             />
-            <Menu.Menu position="right">
-                {!!user && (
-                    <>
-                        <Menu.Item content={`Welcome ${user.Name}`} />
-                        <Menu.Item content="Logout" icon="log out" link onClick={logout} />
-                    </>
-                )}
-            </Menu.Menu>
+            <Menu.Menu position="right">{!!user && <Menu.Item content="Logout" icon="log out" link onClick={logout} />}</Menu.Menu>
         </Menu>
     );
 };
